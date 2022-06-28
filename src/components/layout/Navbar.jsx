78506fdd2caa6d855920ea75ad6b2b8fddbe1148
stephany ramirez <stephany_re@hotmail.com>
2022-06-28 11:40:08 -0700

    navbar links add it

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -8,8 +8,18 @@ function Navbar({title}) {
         <div className="flex-none px-2 mx-2">
           <FaGithub className="inline pr-2 text-3xl"/>
           <Link to = '/' className="text-lg font-bold align-middle">
-
+            {title}
           </Link>
+        </div>
+        <div className="flex-1 px-2 mx-2">
+          <div className="flex justify-end">
+            <Link to ='/' className = 'btn btn-ghost btn-sm rounded-' >
+              Home
+            </Link>
+            <Link to ='/about' className = 'btn btn-ghost btn-sm rounded-' >
+              About
+            </Link>
+          </div>
         </div>
       </div>
 
